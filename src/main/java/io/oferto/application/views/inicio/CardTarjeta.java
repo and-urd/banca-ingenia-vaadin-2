6c742b5dd3605c89c6cd254e1451fc83e6da3421
and-urd <andresurda@gmail.com>
2021-05-26 17:06:32 +0200

    Mejora card Tarjetas

--- src/main/java/io/oferto/application/views/inicio/CardTarjeta.java
+++ src/main/java/io/oferto/application/views/inicio/CardTarjeta.java
@@ -2,6 +2,7 @@ package io.oferto.application.views.inicio;
 
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
@@ -24,26 +25,55 @@ public class CardTarjeta extends VerticalLayout {
         this.setMinWidth("200px");
         this.setMaxWidth("200px");
 
+        // Sombra de la card
+        this.getStyle().set("box-shadow", "5px 10px 8px #888888");
+
+        // color de fondo
+        this.getStyle().set("background-color","#E8DAEF");
+
+        // Borde
         this.getStyle().set("border", "1px solid #800080");
         this.getStyle().set("border-radius",  "20px");
-//        this.getStyle().set("border-color", "#800080");
 
 
-        H2 hEntidad =new H2(nombreEntidad);
+        H3 hEntidad =new H3(nombreEntidad);
         hEntidad.setWidthFull();
+        hEntidad.getStyle().set("fontWeight", "bold");
 
 
-        H2 hSaldo = new H2(saldo.toString() + " €");
+
+        H3 hSaldo = new H3(saldo.toString() + " €");
         hSaldo.getStyle().set("color", "#800080");
+        hSaldo.getStyle().set("margin","11px 0px 11px 0px");
 
 
 
         HorizontalLayout layoutTarjeta = new HorizontalLayout();
 
         H3 hNumeroTarjeta = new H3(numeroTarjeta);
-        H3 hTipoTarjeta = new H3(tipoTarjeta);
+        hNumeroTarjeta.getStyle().set("margin", "0px 0px 0px 27px");
+//        H3 hTipoTarjeta = new H3(tipoTarjeta);
 
-        layoutTarjeta.add(hTipoTarjeta, hNumeroTarjeta);
+        Image imagenIcono = new Image();
+        switch(tipoTarjeta){
+            case "Débito":
+                imagenIcono = new Image("images/icons8-visa-40.png", "Visa");
+                imagenIcono.setWidth("30px");
+                imagenIcono.setHeight("30px");
+                break;
+            case "Crédito":
+                imagenIcono = new Image("images/icons8-mastercard-40.png", "MasterCard");
+                imagenIcono.setWidth("30px");
+                imagenIcono.setHeight("30px");
+                break;
+            default:
+                break;
+        }
+
+
+
+        layoutTarjeta.add(imagenIcono, hNumeroTarjeta);
+
         add(hEntidad , hSaldo, layoutTarjeta);
     }
 }
