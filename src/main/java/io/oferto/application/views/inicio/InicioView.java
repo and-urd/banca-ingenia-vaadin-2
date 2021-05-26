fc95eea6fddb882ea60f3ef015581b65d5f1d549
roriol <rafael.oriol.pedrosa@gmail.com>
2021-05-26 11:41:58 +0200

    PanelMovimientosInicio creado InicioView modificado

--- src/main/java/io/oferto/application/views/inicio/InicioView.java
+++ src/main/java/io/oferto/application/views/inicio/InicioView.java
@@ -1,5 +1,10 @@
 package io.oferto.application.views.inicio;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -9,20 +14,42 @@ import io.oferto.application.views.main.MainView;
 
 @RouteAlias(value = "", layout = MainView.class)
 @Route(value = "inicio", layout = MainView.class)
-@PageTitle("Inicio")
+@PageTitle("Bienvenido/a a tu banca")
 public class InicioView extends VerticalLayout {
 
-    private final MovimientoService movimientoService;
+    private MovimientoService movimientoService;
+    private HorizontalLayout toolBarLayout;
 
     public InicioView(MovimientoService movimientoService){
         this.movimientoService = movimientoService;
 
+        this.setSizeFull();
+        this.setPadding(true);
 
-        PanelTarjetasInicio panelTarjetasInicio = new PanelTarjetasInicio();
         PanelMovimientosInicio panelMovimientosInicio = new PanelMovimientosInicio(movimientoService);
-        this.add(panelTarjetasInicio, panelMovimientosInicio);
+        PanelTarjetasInicio panelTarjetasInicio = new PanelTarjetasInicio();
 
+        add(panelTarjetasInicio);
+        createPanelMovimientoLayout();
+        add(panelMovimientosInicio);
     }
 
+    private void createPanelMovimientoLayout(){
+        toolBarLayout = new HorizontalLayout();
+        toolBarLayout.setWidthFull();
+
+        H3 h3Movimientos = new H3("Movimientos");
+        h3Movimientos.getElement().getStyle().set("margin-right", "auto");
+
+        NativeButton button = new NativeButton(
+                "Ver Más");
+        button.addClickListener(e ->
+                button.getUI().ifPresent(ui ->
+                        ui.navigate("movimientos"))
+        );
+
+        toolBarLayout.add(h3Movimientos, button);
+        add(toolBarLayout);
+    }
 
 }
--- src/main/java/io/oferto/application/views/inicio/PanelMovimientosInicio.java
+++ src/main/java/io/oferto/application/views/inicio/PanelMovimientosInicio.java
@@ -1,19 +1,95 @@
 package io.oferto.application.views.inicio;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.provider.SortDirection;
+import io.oferto.application.backend.modelbanca.Movimiento;
 import io.oferto.application.backend.servicebanca.MovimientoService;
 import io.oferto.application.views.movimientos.MovimientosView;
+import io.oferto.application.views.tarjetas.TarjetasView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PanelMovimientosInicio extends HorizontalLayout {
 
-    private final MovimientoService movimientoService;
+    private MovimientoService movimientoService;
+    private Grid<Movimiento> movimientoGrid = new Grid<>(Movimiento.class);
+    private List<Movimiento> movimientoList;
+    private ListDataProvider<Movimiento> movimientoListDataProvider;
 
     public PanelMovimientosInicio(MovimientoService movimientoService) {
+        this.setSizeFull();
+        this.setPadding(true);
+
         this.movimientoService = movimientoService;
 
-        MovimientosView movimientosView = new MovimientosView(movimientoService);
+        loadData();
 
-        this.add(movimientosView);
+        configureGrid();
     }
+
+    private void loadData(){
+        try{
+            this.movimientoList = movimientosReducidos();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private List<Movimiento> movimientosReducidos(){
+        this.movimientoList = movimientoService.recuperaTodosMovimientos();
+        List<Movimiento> movimientoResultado = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            movimientoResultado.add(movimientoList.get(i));
+        }
+        return movimientoResultado;
+    }
+
+    private void loadGrid(){
+        movimientoListDataProvider = DataProvider.ofCollection(this.movimientoList);
+        movimientoListDataProvider.setSortOrder(Movimiento::getFechaOperacion, SortDirection.DESCENDING);
+
+        movimientoGrid.setDataProvider(movimientoListDataProvider);
+
+    }
+
+    private void configureGrid(){
+        loadGrid();
+        movimientoGrid.setSizeFull();
+
+        movimientoGrid.setColumns("numTarjeta");
+        movimientoGrid.getColumnByKey("numTarjeta").setHeader("Nº Tarjeta").setVisible(false);
+
+        movimientoGrid.addComponentColumn(item ->{
+            Icon icon;
+            if(item.getCantidad() < 0.0){
+                icon = VaadinIcon.CHEVRON_CIRCLE_DOWN_O.create();
+                icon.setColor("red");
+            }else{
+                icon = VaadinIcon.CHEVRON_CIRCLE_UP_O.create();
+                icon.setColor("green");
+            }
+            return icon;
+        }).setKey("icon").setHeader("").setFlexGrow(0).setWidth("60px");
+        movimientoGrid.addColumn(item -> TarjetasView.tarjetaUltimosDigitos(item.getNumTarjeta())).setHeader("Nº Tarjeta").setSortable(true);
+        movimientoGrid.addColumn(Movimiento::getCantidad).setHeader("Cantidad").setSortable(true);
+        movimientoGrid.addColumn(Movimiento::getConcepto).setHeader("Concepto").setSortable(true);
+        movimientoGrid.addColumn(Movimiento::getFechaOperacion).setHeader("Fecha").setSortable(true);
+
+        movimientoGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                GridVariant.LUMO_NO_ROW_BORDERS,
+                GridVariant.LUMO_ROW_STRIPES);
+
+        add(movimientoGrid);
+    }
+
 }
