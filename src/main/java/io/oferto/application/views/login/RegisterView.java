7470ef5ba3365754f5708b5f76adadccdc958fd7
roriol <rafael.oriol.pedrosa@gmail.com>
2021-05-28 17:23:16 +0200

    RegisterView mejorado más

--- src/main/java/io/oferto/application/views/login/RegisterView.java
+++ src/main/java/io/oferto/application/views/login/RegisterView.java
@@ -4,9 +4,11 @@ package io.oferto.application.views.login;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -21,9 +23,10 @@ public class RegisterView extends VerticalLayout {
     public RegisterView(AuthService authService) {
         this.authService = authService;
 
-        setHeightFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setAlignItems(Alignment.CENTER);
+        setPadding(false);
+//        setHeightFull();
+//        setJustifyContentMode(JustifyContentMode.CENTER);
+//        setAlignItems(Alignment.CENTER);
 
         add(registerComponent());
     }
@@ -31,8 +34,22 @@ public class RegisterView extends VerticalLayout {
 
     public Component registerComponent() {
 
-        Image logo = new Image("images/logo-banca-ingenia.PNG", "Banca Ingenia");
-        H3 tittle = new H3("Sign up");
+        HorizontalLayout layoutCabecera = new HorizontalLayout();
+        layoutCabecera.setJustifyContentMode(JustifyContentMode.CENTER);
+        layoutCabecera.getStyle().set("padding","30px");
+
+        Image logotipo = new Image("images/logo-banca-ingenia.PNG", "Banca Ingenia");
+        logotipo.setHeight("50px");
+        logotipo.setWidth("50px");
+
+        H1 hTitulo = new H1("Ingenia Bank");
+        hTitulo.getStyle().set("margin", "0px 0px 0px 10px");
+
+        layoutCabecera.add(logotipo, hTitulo);
+        this.setHorizontalComponentAlignment(Alignment.CENTER, layoutCabecera);
+
+//        Image logo = new Image("images/logo-banca-ingenia.PNG", "Banca Ingenia");
+//        H3 tittle = new H3("Sign up");
         TextField username = new TextField("Username");
         PasswordField password1 = new PasswordField("Password");
         PasswordField password2 = new PasswordField("Confirm password");
@@ -44,7 +61,7 @@ public class RegisterView extends VerticalLayout {
 
         VerticalLayout registerLayout = new VerticalLayout();
 
-        registerLayout.add(logo, tittle, username,password1,password2,registerButton);
+        registerLayout.add(layoutCabecera, username, password1, password2, registerButton);
         registerLayout.setHeightFull();
         registerLayout.setJustifyContentMode(JustifyContentMode.CENTER);
         registerLayout.setAlignItems(Alignment.CENTER);
